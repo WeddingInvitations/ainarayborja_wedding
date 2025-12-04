72bc5f2156e1df8ec53751019e6574031a7b8cc0
Oscar Gavira <gaviradiazoscar@gmail.com>
2025-12-04 18:53:22 +0100

    Correcion rutas

--- www/js/firebaseConfig.js
+++ www/js/firebaseConfig.js
@@ -3,9 +3,9 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.5.0/firebas
 
 const firebaseConfig = {
   apiKey: "AIzaSyDvzoGMS3fTCJwGP7nM_ov9gAP57hP1m00",
-  authDomain: "sarayjordiwedding.firebaseapp.com",
-  projectId: "sarayjordiwedding",
-  storageBucket: "sarayjordiwedding.firebasestorage.app",
+  authDomain: "ainarayborjawedding.firebaseapp.com",
+  projectId: "ainarayborjawedding",
+  storageBucket: "ainarayborjawedding.firebasestorage.app",
   messagingSenderId: "829826807273",
   appId: "1:829826807273:web:f3f5d75f8593b31d680778",
   measurementId: "G-VCNDK2XG49"
